77c26abf90e93d3178dca026d0b8b4fe8f3b7919
Arima057nn <tjendung97pain@gmail.com>
2024-06-04 23:42:22 +0700

    add token axios

--- src/services/axios-cilent.ts
+++ src/services/axios-cilent.ts
@@ -18,10 +18,10 @@ axiosClient.interceptors.response.use(
 
 axiosClient.interceptors.request.use(
   async (config) => {
-    // const token = await AsyncStorage.getItem("token");
-    // if (token) {
-    //   config.headers.Authorization = `Bearer ${token}`;
-    // }
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   function (error) {
